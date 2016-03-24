179a112bc0f89eb0499190f6a72636419a2afe68
Ruslan Khakimov <izuick@gmail.com>
2016-03-25 00:39:05 +0500

    Eating food

--- js/game.js
+++ js/game.js
@@ -2,11 +2,18 @@ var game = new Phaser.Game(600, 600, Phaser.CANVAS, 'node-keeper', { preload: pr
 
 function preload() {
     game.load.image('guy', 'assets/guy.png');
+    game.load.image('pizza', 'assets/pizza.png');
+    game.load.image('beer', 'assets/beer.png');
     game.load.spritesheet('button', 'assets/button.png', 32, 32);
 }
 
 var maxButtons = 8;
 var nodes = [ 'acl', 'core', 'embed', 'feeds', 'recommends', 'search', 'stash', 'auth' ];
+var foods = [ 'pizza', 'beer' ];
+var bonusRadius = 100;
+var bonusTimeout = 5000;
+var bonusSpeedEffect = 15;
+
 var nodeFailDelayMin = 500;
 var nodeFailDelayMax = 2500;
 var nodeFailDelta = 100;
@@ -21,12 +28,27 @@ var isGameOver = false;
 
 var guy;
 var buttons;
+var bonuses;
+
 var score; 
 var scoreText;
 
+function createBonus( foodName, index ){
+    var arc = Math.random() * Math.PI * 2;
+    var x = bonusRadius * Math.cos( arc ) + game.width / 2;
+    var y = bonusRadius * Math.sin( arc ) + game.height / 2;
+    var bonus = game.add.sprite( x, y, foodName );
+    bonus.index = index;
+    game.physics.enable( bonus, Phaser.Physics.ARCADE);
+    bonus.anchor.set(0.5);
+    bonus.foodName = foodName;
+    bonus.name = 'bonus'; 
+    return bonus;
+}
+
 function createButton( x, y, nodeName ){
     var btn = game.add.sprite(x, y, 'button');        
-    btn.frame = 2;
+    btn.frame = 0;
     btn.name = 'button';
     game.physics.enable( btn, Phaser.Physics.ARCADE );
     btn.body.immovable = true;
@@ -34,34 +56,23 @@ function createButton( x, y, nodeName ){
     btn.text = game.add.text( x, y - 25, nodeName, buttonsFontStyle);
     btn.text.anchor.set(0.5);
     
+    btn.restartAnimation = btn.animations.add('restart', [2,3,4,5,6,7,8,9], 12, false);
+    btn.restartAnimation.onComplete.add(                
+        function(){ 
+            btn.frame = 0;
+            btn.isBtnEnabled = true; 
+            btn.triggered = false;
+            score++;
+            scoreText.text = 'Score: ' + score;
+        },this);
+                
     btn.turnOff = function(){
-        if( !btn.triggered ){
-            btn.frame = 1;
-            game.time.events.add(
-                Phaser.Timer.SECOND * 0.2, 
-                function(){ 
-                    btn.frame = 0;
-                    btn.isBtnEnabled = false; 
-                    btn.triggered = false;
-                },
-            this);            
-            btn.triggered = true;
-        }
+        btn.frame = 1;
+        btn.isBtnEnabled = false;             
     }
     btn.turnOn = function(){
         if( !btn.triggered ){
-            btn.frame = 1;
-            game.time.events.add(
-                Phaser.Timer.SECOND * 0.2, 
-                function(){ 
-                    btn.frame = 2;
-                    btn.isBtnEnabled = true; 
-                    btn.triggered = false;
-                    score++;
-                    scoreText.text = 'Score: ' + score;
-                },
-                
-            this);            
+            btn.restartAnimation.play();                        
             btn.triggered = true;
         }
     }
@@ -77,11 +88,10 @@ function createGuy( x, y ){
     return guy;
 }
 
-function doNodeFail(){  
-    console.log(nodeFailDelay);
+function doNodeFail(){
     var turnedOn = buttons.filter( function( b ){ return b.isBtnEnabled; } );
     
-    if( turnedOn.length <= buttons.length / 2 ){
+    if( turnedOn.length === 0 ){
         gameOver();
         return;
     }
@@ -94,10 +104,16 @@ function doNodeFail(){
     game.time.events.add( nodeFailDelay, doNodeFail, this );
 }
 
-function destoryButton( btn ){
+function destroyButton( btn ){
     btn.text.destroy();
     btn.destroy();
 }
+
+function destroyBonus( bonus ){
+    bonuses.splice( bonus.index, 1 );
+    bonus.destroy();
+}
+
 function gameOver(){
     game.stage.backgroundColor = '#000';
     game.time.events.add(
@@ -110,7 +126,8 @@ function gameOver(){
     isGameOver = true;
     
     guy.destroy();
-    buttons.forEach( destoryButton );
+    buttons.forEach( destroyButton );
+    bonuses.forEach( function( b ){ b.destroy(); } );
     scoreText.destroy();
     
     var text = game.add.text( game.width / 2, game.height / 2, 'GAME OVER\n Your score: ' + score, gameOverFontStyle);
@@ -131,28 +148,54 @@ function createButtons(){
         );        
     })   
 }
+
+
+function placeBonus(){  
+    if( !isGameOver ){
+        var freeBonuses 
+            = foods.filter( function( f ){ 
+                return bonuses.filter( function( b ){
+                    return b.foodName === f;
+                } ).length === 0;
+            } );
+
+        if( freeBonuses.length > 0 ){
+            bonuses.push( 
+                createBonus( freeBonuses[ game.rnd.integerInRange( 0, freeBonuses.length - 1 ) ], bonuses.length ) 
+            );        
+        }        
+    }
+}
+
 function create() {
     score = 0;    
     buttons = [];
+    bonuses = [];
     nodeFailDelay = nodeFailDelayMax;
     
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.stage.backgroundColor = '#ccc';
     
     guy = createGuy( game.width / 2, game.height / 2);
-    createButtons();
+    createButtons();   
     
     game.time.events.add( nodeFailDelayMax, doNodeFail, this );    
+    game.time.events.loop( bonusTimeout, placeBonus, this);
     
-    scoreText = game.add.text( game.width / 2, 12, 'Score:' + score, buttonsFontStyle);
+    scoreText = game.add.text( game.width / 2, 20, 'Score:' + score, buttonsFontStyle);
     scoreText.anchor.set(0.5);
+    
     
 }
 
 function update() {
     if( !isGameOver ){
         buttons.forEach( function( b ){
-            game.physics.arcade.collide(guy, b, collisionHandler, null, this);        
+            game.physics.arcade.collide(guy, b, processNodeCollision, null, this);        
+        })
+        
+        bonuses.forEach( function( b ){
+            game.physics.arcade.collide(guy, b, processBonusCollision, null, this);        
         })
 
         if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
@@ -175,7 +218,7 @@ function update() {
     }
 }
 
-function collisionHandler (obj1, obj2) {
+function processNodeCollision(obj1, obj2) {
     var btn;
     if( obj1.name === 'button' ) btn = obj1;
     if( obj2.name === 'button' ) btn = obj2;
@@ -183,6 +226,16 @@ function collisionHandler (obj1, obj2) {
     if( !btn.isBtnEnabled ) { 
         btn.turnOn();
     }
+    
+    return false;
+}
+function processBonusCollision(obj1, obj2) {
+    var bonus;
+    if( obj1.name === 'bonus' ) bonus = obj1;
+    if( obj2.name === 'bonus' ) bonus = obj2;
+
+    guy.speed += bonusSpeedEffect;
+    destroyBonus( bonus );
     
     return false;
 }
---
+++ readme.md
@@ -0,0 +1,6 @@
+#Node Keeper Game#
+
+Keep working at least one module! Use arrows to move, eat food to encrease speed.
+
+#Play#
+http://zuick.github.io/node-keeper-game/
